83ed78cf8a38b36220ff0e081e0d03a8dd44f23a
TBM <thegalaxygamesyt@gmail.com>
2020-04-01 21:03:12 +0100

    done some stuff
    must make the stack work with the instructions (all values should be on first index)

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from instructions import check
-
 stack = []
 local_variables = []
 
 instruction = ""
 
+def check(instruction):
+    instrct = instruction.lower()
+    if instrct == "iconst_m1":
+        stack.append(-1)
+    if instrct == "iconst_0":
+        stack.append(0)
+    if instrct == "iconst_1":
+        stack.append(1)
+    if instrct == "iconst_2":
+        stack.append(2)
+    if instrct == "iconst_3":
+        stack.append(3)
+    if instrct == "iconst_4":
+        stack.append(4)
+    if instrct == "iconst_5":
+        stack.append(5)
 
 while True:
     instruction = input("Enter Instruction: ")
