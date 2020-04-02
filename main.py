a260524e9c2f116b6b2b49b3936ad6f812986690
Darki <50775247+DarkiBoi@users.noreply.github.com>
2020-04-02 12:13:29 +0200

    cringe bro

--- main.py
+++ main.py
@@ -10,48 +10,49 @@ def check(instrct):
     instrct = instrct.lower()
     if instrct == "iconst_m1":
         stack.insert(0, -1)
-    if instrct == "iconst_0":
+    elif instrct == "iconst_0":
         stack.insert(0, 0)
-    if instrct == "iconst_1":
+    elif instrct == "iconst_1":
         stack.insert(0, 1)
-    if instrct == "iconst_2":
+    elif instrct == "iconst_2":
         stack.insert(0, 2)
-    if instrct == "iconst_3":
+    elif instrct == "iconst_3":
         stack.insert(0, 3)
-    if instrct == "iconst_4":
+    elif instrct == "iconst_4":
         stack.insert(0, 4)
-    if instrct == "iconst_5":
+    elif instrct == "iconst_5":
         stack.insert(0, 5)
-    if instrct == "pop":
+    elif instrct == "pop":
         stack.pop(0)
-    if instrct == "pop2":
+    elif instrct == "pop2":
         stack.pop(0)
         stack.pop(0)
-    if instrct == "aload_0":
+    elif instrct == "aload_0":
         stack.insert(0, local_variables[0])
-    if instrct == "aload_1":
+    elif instrct == "aload_1":
         stack.insert(0, local_variables[1])
-    if instrct == "aload_2":
+    elif instrct == "aload_2":
         stack.insert(0, local_variables[2])
-    if instrct == "aload_3":
+    elif instrct == "aload_3":
         stack.insert(0, local_variables[3])
-    if instrct == "swap":
+    elif instrct == "swap":
         stack[0], stack[1] = stack[1], stack[0]
-    if instrct == "dup":
+    elif instrct == "dup":
         stack.insert(0, stack[0])
-    if instrct == "dup_x1":
+    elif instrct == "dup_x1":
         stack.insert(2, stack[0])
-    if instrct == "dup_x2":
+    elif instrct == "dup_x2":
         stack.insert(3, stack[0])
-    if instrct == "dup2":
+    elif instrct == "dup2":
         stack.insert(0, stack[1])
         stack.insert(0, stack[1])
-    if instrct == "dup2_x1":
+    elif instrct == "dup2_x1":
         stack.insert(3, stack[0])
         stack.insert(4, stack[1])
-    if instrct == "dup2_x2":
+    elif instrct == "dup2_x2":
         stack.insert(4, stack[0])
         stack.insert(5, stack[1])
+
 
 while True:
     instruction = input("Enter Instruction: ")
